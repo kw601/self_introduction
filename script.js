6b992ab5e7d90941bdde6af86c11658290945bf5
Gyungwon Na <nkw601@naver.com>
2024-07-04 16:31:19 +0900

    완성

--- script.js
+++ script.js
@@ -1,20 +1,15 @@
-const profilePhoto = document.querySelector('.profile-photo');
-    // const profilePhoto = document.getElementsByClassname('.profile-photo')[0];
-    
-    
-profilePhoto.addEventListener('click', () => {
-        //callback 함수에 인자 필요 없어서 (event) X
-        // if(document.body.className == 'dark-mode'){
-        //     document.body.className = '';
-        // } else {
-        //     document.body.className = 'dark-mode';
-        // }
-    
-        document.body.classList.toggle('dark-mode');
-        //toggle: 있으면 없게, 없으면 있게, 위와 같은 뜻
+const profilePhoto = document.querySelector(".profile-photo")
+
+profilePhoto.addEventListener("click", () => {
+    // if(document.body.className == 'dark-mode'){
+    //     document.body.className = ''
+    // }else{
+    //     document.body.className = 'dark-mode'
+    // }
+    document.body.classList.toggle("dark-mode");
 });
 
-//이렇게 새로운 요소를 넣으면 이벤트리스너를 그 밑으로 옮겨줘야 함
+
 fetch("https://m.search.naver.com/p/csearch/content/apirender.nhn?where=nexearch&pkid=387&u2=20010601&q=%EC%83%9D%EB%85%84%EC%9B%94%EC%9D%BC+%EC%9A%B4%EC%84%B8&u1=f&u3=solar&u4=5&_=1720073662874")
     .then(response => response.json()) // 응답을 JSON으로 파싱
     .then(data => {
@@ -26,105 +21,110 @@ fetch("https://m.search.naver.com/p/csearch/content/apirender.nhn?where=nexearch
         console.log(fortune); // 추출한 텍스트 출력
         console.log(fortuneText); // 추출한 텍스트 출력
 
-        const fortuneSectionTitle = document.createElement('h2');
-        fortuneSectionTitle.textContent = "오늘의 운세";
-
-        const fortuneE = document.createElement('h3');
+        const fortuneE = document.createElement("h3");
         fortuneE.style.margin = 0;
         fortuneE.textContent = fortune;
 
-        const fortuneTextE = document.createElement('p');
+        const fortuneTextE = document.createElement("p");
         fortuneTextE.textContent = fortuneText;
 
-        const fortuneSection = document.createElement('section');
+        const fortuneSectionTitle = document.createElement('h2');
+        fortuneSectionTitle.textContent = "오늘의 운세"
+
+        const fortuneSection = document.createElement("section")
+        
         fortuneSection.append(fortuneSectionTitle);
         fortuneSection.append(fortuneE);
         fortuneSection.append(fortuneTextE);
 
-        const contactSection = document.querySelector('.contact');
-        contactSection.before(fortuneSection);
-
-        const sections = document.querySelectorAll('.right section');
-        let currentIndex = 0;
+        const contactS = document.querySelector(".contact");
+        contactS.before(fortuneSection)
+    })
+    .finally(() => {
         
-        const showAfterSection = ()=> {
+        const sections = document.querySelectorAll(".right section");
+
+        let currentIndex = 0;
+
+        const showAfterSection = ()=>{
+            sections.forEach((section)=>{section.style.display = 'none'})
+            if(currentIndex == sections.length - 1){
+                currentIndex = 0;
+            }else{
+                currentIndex++;
+            }
+            sections[currentIndex].style.display = 'flex'
+        }
+
+        const showBeforeSection = ()=>{
+            sections.forEach((section)=>{section.style.display = 'none'})
+            if(currentIndex == 0){
+                currentIndex = sections.length - 1;
+            }else{
+                currentIndex-- ;
+            }
+            sections[currentIndex].style.display = 'flex'
+        }
+
+        let intervalId = setInterval(showAfterSection, 3000);
+
+        const resetInterval = () => {
+            clearInterval(intervalId)
+            intervalId = setInterval( showAfterSection, 3000);
+        }
+
+        sections.forEach((section,index)=>{
             const iconContainer = document.querySelector(".icon-container");
-            sections.forEach((section)=>{section.style.display = 'none';})
-            currentIndex = (currentIndex + 1) % (sections.length);
-            sections[currentIndex].style.display = 'flex';
-        }
 
-        const showBeforeSection = ()=> {
-            sections.forEach((section)=>{section.style.display = 'none';})
-            currentIndex = (currentIndex - 1 + sections.length) % (sections.length);
-            sections[currentIndex].style.display = 'flex';
-        }
-            
-        let intervalID = setInterval(showAfterSection, 3000);
-             
-        const resetInverval = () => {
-            clearInterval(intervalID);
-            intervalID = setInterval(showAfterSection, 3000);
-        };
-            
-        sections.forEach((section, index)=>{
-            section.addEventListener('click', (event)=>{
-                const sectionWidth = section.offsetWidth;    
-                const clickx = event.clientX - section.getBoundingClientRect().left;    
-                if (clickx < (sectionWidth / 3)){
-                    showBeforeSection();
-                    resetInverval();
-                    iconContainer.innerHTML = '<i class="fa-solid fa-backward-fast"></i>';
-
-                } else if (clickx > (sectionWidth * 2 / 3)) {
-                    showAfterSection();
-                    resetInverval();
-                    iconContainer.innerHTML = '<i class="fa-solid fa-forward-fast"></i>';
-
-                }else {
-                    if(intervalID){
-                        clearInterval(intervalID);
-                        intervalID = null;
-                        iconContainer.innerHTML = '<i class="fa-solid fa-pause"></i>';
-
-                    } else {
-                        intervalID = setInterval(showAfterSection, 3000);
-                        iconContainer.innerHTML = '<i class="fa-solid fa-play"></i>';
-
-                    }
-                }
-            });
-            section.addEventListener('mousemove', function(event) {
+            section.addEventListener("click", (event) =>{
                 const sectionWidth = section.offsetWidth;
+
                 const clickX = event.clientX - section.getBoundingClientRect().left;
-      
-                // 아이콘 표시 위치 설정
-                iconContainer.style.top = `${event.clientY - 20}px`;
-                iconContainer.style.left = `${event.clientX - 20}px`;
-      
-                // 마우스 위치에 따라 클래스명 변경
-                if (clickX < sectionWidth / 3) {
+
+                if( clickX < sectionWidth / 3 ){
+                    showBeforeSection();
+                    resetInterval();
                     iconContainer.innerHTML = '<i class="fa-solid fa-backward-fast"></i>';
                 } else if (clickX > sectionWidth * 2 / 3) {
+                    showAfterSection();
+                    resetInterval();
                     iconContainer.innerHTML = '<i class="fa-solid fa-forward-fast"></i>';
-                } else {
-                    if (intervalID) {
+                } else{
+                    if(intervalId){
+                        clearInterval(intervalId)
+                        intervalId = null
                         iconContainer.innerHTML = '<i class="fa-solid fa-pause"></i>';
                     }else{
-                        iconContainer.innerHTML = '<i class="fa-solid fa-play"></i>'
-                    }
-                    // 삼항연산자
-                    //iconContainer.innerHTML = intervalID ? '<i class="fa-solid fa-pause"></i>' : '<i class="fa-solid fa-play"></i>';
-                    section.addEventListener('mouseleave', function() {
-                        // 마우스가 섹션을 떠날 때 아이콘 숨김
-                        iconContainer.innerHTML = '';
-                    });
+                        intervalId = setInterval(showAfterSection, 3000)
+                        iconContainer.innerHTML = '<i class="fa-solid fa-play"></i>';
+                    } 
                 }
-            });
-        });
+            })
+            section.addEventListener('mousemove',(event)=>{
+                const sectionWidth = section.offsetWidth;
+
+                const clickX = event.clientX - section.getBoundingClientRect().left;
+                
+                iconContainer.style.top = `${event.clientY - 20}px`;
+                iconContainer.style.left = `${event.clientX - 20}px`;
+                // 마우스 위치에 따라 클래스명 변경
+            if (clickX < sectionWidth / 3) {
+                iconContainer.innerHTML = '<i class="fa-solid fa-backward-fast"></i>';
+            } else if (clickX > sectionWidth * 2 / 3) {
+                iconContainer.innerHTML = '<i class="fa-solid fa-forward-fast"></i>';
+            } else {
+                if (intervalId) {
+                    iconContainer.innerHTML = '<i class="fa-solid fa-pause"></i>';
+                }else{
+                    iconContainer.innerHTML = '<i class="fa-solid fa-play"></i>';
+                }
+            // 삼항연산자
+            //iconContainer.innerHTML = intervalId ? '<i class="fa-solid fa-pause"></i>' : '<i class="fa-solid fa-play"></i>';
+            }
+            })
+            section.addEventListener('mouseleave', ()=>{
+                iconContainer.innerHTML = ''
+            })
+    });
 })
-    
 
-    
-
-    
